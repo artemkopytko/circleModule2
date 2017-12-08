93e4def6daf637fdaf8adc4cad6c40a12c90cb2f
artemkopytko <kopytkoartem@gmail.com>
2017-12-08 12:15:39 +0200

    Added Validation

--- Circle/Program.cs
+++ Circle/Program.cs
@@ -11,14 +11,25 @@ namespace Circle
             CircleClass secondCircle = new CircleClass { radius = 7 };
             CircleClass thirdCircle = new CircleClass { radius = 9 };
             CircleClass fourthCircle = new CircleClass { radius = 11 };
-            Console.WriteLine("Circle №1, radius = {0}, area = {1}", firstCircle.radius, firstCircle.GetArea(firstCircle));
-            Console.WriteLine("Circle №2, radius = {0}, area = {1}", secondCircle.radius, secondCircle.GetArea(secondCircle));
-            Console.WriteLine("Circle №3, radius = {0}, area = {1}", thirdCircle.radius, thirdCircle.GetArea(thirdCircle));
-            Console.WriteLine("Circle №4, radius = {0}, area = {1}", fourthCircle.radius, fourthCircle.GetArea(fourthCircle));
+            if (firstCircle.IsRadiusPositive(firstCircle))
+            {
+                Console.WriteLine("Circle №1, radius = {0}, area = {1}", firstCircle.radius, firstCircle.GetArea(firstCircle));
+            }
+            if (secondCircle.IsRadiusPositive(secondCircle))
+            {
+                Console.WriteLine("Circle №2, radius = {0}, area = {1}", secondCircle.radius, secondCircle.GetArea(secondCircle));
+            }
+            if (thirdCircle.IsRadiusPositive(thirdCircle))
+            {
+                Console.WriteLine("Circle №3, radius = {0}, area = {1}", thirdCircle.radius, thirdCircle.GetArea(thirdCircle));
+            }
+            if (fourthCircle.IsRadiusPositive(fourthCircle))
+            {
+                Console.WriteLine("Circle №4, radius = {0}, area = {1}", fourthCircle.radius, fourthCircle.GetArea(fourthCircle));
+            }
             Console.WriteLine("Total area = {0}", firstCircle.GetArea(firstCircle) + secondCircle.GetArea(secondCircle) + thirdCircle.GetArea(thirdCircle) + fourthCircle.GetArea(fourthCircle));
         }
     }
-
 
     public class CircleClass
     {
@@ -27,6 +38,18 @@ namespace Circle
         public double GetArea(CircleClass circle)
         {
             return Math.PI * circle.radius * circle.radius;
+        }
+
+        public bool IsRadiusPositive(CircleClass circle)
+        {
+            if (circle.radius > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
