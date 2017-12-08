a26da319b5029aebc9519987206d5832fa942c42
artemkopytko <kopytkoartem@gmail.com>
2017-12-08 12:02:55 +0200

    Unit test to check GetArea method

--- Circle/Program.cs
+++ Circle/Program.cs
@@ -7,10 +7,10 @@ namespace Circle
         public static void Main(string[] args)
         {
             Console.WriteLine("Counting circle area");
-            Circle firstCircle = new Circle { radius = 5 };
-            Circle secondCircle = new Circle { radius = 7 };
-            Circle thirdCircle = new Circle { radius = 9 };
-            Circle fourthCircle = new Circle { radius = 11 };
+            CircleClass firstCircle = new CircleClass { radius = 5 };
+            CircleClass secondCircle = new CircleClass { radius = 7 };
+            CircleClass thirdCircle = new CircleClass { radius = 9 };
+            CircleClass fourthCircle = new CircleClass { radius = 11 };
             Console.WriteLine("Circle №1, radius = {0}, area = {1}", firstCircle.radius, firstCircle.GetArea(firstCircle));
             Console.WriteLine("Circle №2, radius = {0}, area = {1}", secondCircle.radius, secondCircle.GetArea(secondCircle));
             Console.WriteLine("Circle №3, radius = {0}, area = {1}", thirdCircle.radius, thirdCircle.GetArea(thirdCircle));
@@ -19,11 +19,12 @@ namespace Circle
         }
     }
 
-    public class Circle
+
+    public class CircleClass
     {
         public double radius = 0.0;
 
-        public double GetArea(Circle circle)
+        public double GetArea(CircleClass circle)
         {
             return Math.PI * circle.radius * circle.radius;
         }
