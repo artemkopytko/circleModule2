9a7e17f9f114ed150bf5f1bfd53ef57402a18127
artemkopytko <kopytkoartem@gmail.com>
2017-12-08 12:20:30 +0200

    Unit test to check radius > 0

--- UnitTestAreaIsCorrect/UnitTest5.cs
+++ UnitTestAreaIsCorrect/UnitTest5.cs
@@ -1,1 +1,23 @@
-﻿
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Circle;
+
+
+namespace UnitTestIsRadiusCorrect
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestAreaIsRadCor()
+        {
+            int radius = 5;
+            double expected = true;
+
+            CircleClass circle = new CircleClass { radius = 5 };
+
+            Assert.AreEqual(expected, circle.IsRadiusPositive(circle));
+
+        }
+    }
+}
