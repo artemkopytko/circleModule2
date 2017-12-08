b7138966012244456a25251f91a3d107ea7a2587
artemkopytko <kopytkoartem@gmail.com>
2017-12-08 12:04:05 +0200

    Unit test fix

---
+++ UnitTestAreaIsCorrect/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Circle;
+
+
+namespace UnitTestAreaIsCorrect5
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestAreaIsCorrect5()
+        {
+            int radius = 5;
+            double expected = radius * radius * Math.PI;
+
+            CircleClass circle = new CircleClass { radius = 5 };
+
+            Assert.AreEqual(expected, circle.GetArea(circle));
+
+        }
+    }
+}
