695cea05b6f072dff0ee6b98834fc374afcdacbb
artemkopytko <kopytkoartem@gmail.com>
2017-12-08 11:42:48 +0200

    Made code readable with refactoring

--- Circle/Program.cs
+++ Circle/Program.cs
@@ -6,26 +6,26 @@ namespace Circle
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Circle area");
-            Circle c1 = new Circle { r = 5 };// c1 = firstCircle, r = Radius
-            Circle c2 = new Circle { r = 7 };
-            Circle c3 = new Circle { r = 9 };
-            Circle c4 = new Circle { r = 11 };
-            Console.WriteLine("Circle 1, rad equals {0}, area equals {1}", c1.r, c1.getArea(c1));
-            Console.WriteLine("Circle 1, rad equals {0}, area equals {1}", c2.r, c1.getArea(c2));
-            Console.WriteLine("Circle 1, rad equals {0}, area equals {1}", c3.r, c1.getArea(c3));
-            Console.WriteLine("Circle 1, rad equals {0}, area equals {1}", c4.r, c1.getArea(c4));
-            Console.WriteLine("Area sum eqals {0}", c1.getArea(c1) + c2.getArea(c2) + c3.getArea(c3) + c4.getArea(c4));
+            Console.WriteLine("Counting circle area");
+            Circle firstCircle = new Circle { radius = 5 };
+            Circle secondCircle = new Circle { radius = 7 };
+            Circle thirdCircle = new Circle { radius = 9 };
+            Circle fourthCircle = new Circle { radius = 11 };
+            Console.WriteLine("Circle №1, radius = {0}, area = {1}", firstCircle.radius, firstCircle.GetArea(firstCircle));
+            Console.WriteLine("Circle №2, radius = {0}, area = {1}", secondCircle.radius, secondCircle.GetArea(secondCircle));
+            Console.WriteLine("Circle №3, radius = {0}, area = {1}", thirdCircle.radius, thirdCircle.GetArea(thirdCircle));
+            Console.WriteLine("Circle №4, radius = {0}, area = {1}", fourthCircle.radius, fourthCircle.GetArea(fourthCircle));
+            Console.WriteLine("Total area = {0}", firstCircle.GetArea(firstCircle) + secondCircle.GetArea(secondCircle) + thirdCircle.GetArea(thirdCircle) + fourthCircle.GetArea(fourthCircle));
         }
     }
 
     public class Circle
     {
-        public double r = 0.0; // radius
+        public double radius = 0.0;
 
-        public double getArea(Circle c)// <- circle & Get
+        public double GetArea(Circle circle)
         {
-            return Math.PI * c.r * c.r;
+            return Math.PI * circle.radius * circle.radius;
         }
     }
 }
